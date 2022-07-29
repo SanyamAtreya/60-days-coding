ee336fc4d4015bbe52853e62474cfd98c18c0680
EZIO-17 <samatreya@gmail.com>
2022-07-29 23:41:02 +0530

    Valid anagrams - leetcode

--- valid_anagrams.py
+++ valid_anagrams.py
@@ -3,23 +3,18 @@ class Solution(object):
         if len(s) != len(t):
             return False
         
-        #Declaring dictionary to store count  
+        
         count = collections.defaultdict(int)
         
-        #Taking the count of the characters in the first string
         for val in s:
             count[val]+=1
         
-        #Iterating the charaters from the second string and reducing the count
         for val in t:
             count[val]-=1
             
-        #Iterating over dictionary
         for item in count:
-            #If count is having non zero then return False
             if count[item] > 0:
                 return False
             
-        #All good, returning True
         return True      
         
