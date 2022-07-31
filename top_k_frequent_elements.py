46ea58a225aac966a60670c6f478e30c3692fd58
EZIO-17 <samatreya@gmail.com>
2022-07-31 16:42:55 +0530

    alternate sol with heap

--- top_k_frequent_elements.py
+++ top_k_frequent_elements.py
@@ -1,25 +1,11 @@
 class Solution(object):
     def topKFrequent(self, nums, k):
-        res={}
-        lst=[]
-        len_nums = len(nums)
-        for i in range(len_nums):
-            count=0
-            for j in range(len_nums):
-                
-
-                if nums[i] == nums[j]:
-                    
-                    count+=1
-
-            if nums[i] not in res:
-                
-                res[nums[i]]=count
-        sort_data = sorted(res.items(), key=lambda x: x[1], reverse=True)
-        
-        for i in sort_data:
-            
-            if len(lst) < k:
-                lst.append(i[0])
-
-        return lst
+        c = Counter(nums)            
+        h = []
+        for i in c:
+            heappush(h, (-c[i],i))
+        res = []
+        for i in range(k):
+            kv = heappop(h)
+            res.append(kv[1])
+        return res
