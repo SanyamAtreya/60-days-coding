322d3064eb86997d84304ad4f8babca3846840db
EZIO-17 <samatreya@gmail.com>
2022-08-04 23:35:35 +0530

    Longest consecutive sequence - leetcode

---
+++ longest_consecutive_string.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def longestConsecutive(self, nums):  
+        s = set(nums)
+        long=0 # keeping track of longest sequence
+        for i in nums:
+            if (i-1) not in s:
+                l = 0
+                while (i+l) in s:
+                    l+=1
+                long = max(l,long)
+        return long 
