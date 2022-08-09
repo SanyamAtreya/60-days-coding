59b0624fc13fcbf09419a28cbf48cd42aa9d599b
EZIO-17 <samatreya@gmail.com>
2022-08-09 17:51:28 +0530

    Valid Palindrome - leetcode

--- valid_palindrome.py
+++ valid_palindrome.py
@@ -1,7 +1,18 @@
-
-
-
-
+class Solution(object):
+    def isPalindrome(self, s):
+        l2 = ''.join(ch for ch in s if ch.isalnum())
+      
+        l2=l2.lower()
+        # s.replace('a', '')
+        r=len(l2)-1
+        l=0
+        pala=True
+        while(l<=r):
+            if(l2[l]!=l2[r]):
+                pala=False
+            l=l+1
+            r=r-1
+        return pala
 
 
 
