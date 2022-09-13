26a047aae557fb05b09a5c606e7c15b88f0bce45
unknown <samatreya@gmail.com>
2022-09-13 23:15:57 +0530

    Factorial using recursion

---
+++ factorial.py
@@ -0,0 +1,13 @@
+
+
+def factorial(x):
+    if x == 1:
+        return 1
+    else:
+        return (x * factorial(x-1))
+
+
+num = 7
+
+result = factorial(num)
+print("The factorial of", num, "is", result)
